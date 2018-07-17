e20599268fee8dcd3acb3f9f1012a683270ccefb
Daniel Siddel <daniel@codify.com>
2018-07-17 15:40:46 +1000

    added arm template sku finder script

---
+++ PowerShell/FindARMTemplateSKUforVM.ps1
@@ -0,0 +1,9 @@
+﻿$locname = "Australia East"
+$pubname = "RedHat"
+$offername = "RHEL"
+$skuname = "7.4"
+$version = "8.1.0"
+#Get-AzureRMVMImagePublisher -Location $locName | Select PublisherName
+#Get-AzureRMVMImageOffer -Location $locName -Publisher $pubName | Select Offer
+#Get-AzureRMVMImageSku -Location $locName -Publisher $pubName -Offer $offerName | Select Skus
+Get-AzureRMVMImage -Location $locName -Publisher $pubName -Offer $offerName -Sku $skuName | Select Version
